ed0de8e0750d16ea05a62ff32ab1dac496fef0bf
TheBamass <126826170+TheBamass@users.noreply.github.com>
2023-03-11 21:49:09 +0100

    Create Blazer.cpp

---
+++ Blazer.cpp
@@ -0,0 +1,72 @@
+// carrots.cpp 
+// lidting 2,3
+#include <isostream>
+int main()
+{
+  using namespace std;
+  int tomatoes;
+  tomatoes;
+  tomatoes = 25;
+  cout <<  "ihave";
+  cout <<  tomatoes
+  cout << " tomatoes ";
+  cout << endl;
+   tomatoes = tomatoes +25;
+  cout << " nom ,nom, know i have" << tomamtoes << "tomamtoes" 
+	  endl;
+	return 0; 
+}
+
+// sqrt.cpp
+// listing 2,4
+#include <isotream>
+#include <cmath g++ sqrt.C /im>
+using namespace std;
+int main 
+[
+dubble area;
+	cout << "her area is arund 3.5 cm2"
+	cin >> area:
+	dubble side;
+	side   sqrt(area)
+	cout  << "equals to 5.7 cm2" << side 
+	cout <<  "meter." << endl;
+	cout  << "so facinating to kinow " << end dnl; 
+	return 0;
+	}
+	
+
+	// myfunc.cpp
+	// listing 2,5
+	
+	int main()
+	#include <isostream>
+	void polly (int)
+	int main()
+	
+	}
+	using namespace std;
+	polly(3);
+	cout << ("4");
+	int count;
+	cin >< cout;
+	polly ("count");
+	cout <<"done!";
+	 <<endl;
+	return =;
+	
+	{
+	void polly (int n 
+		    
+        {
+		
+		
+		
+		                                             
+			    
+			    
+			    
+		
+	
+	
+     
